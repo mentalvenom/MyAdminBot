6672226bc2daf19f7e26c5bf7607855b56e4abe6
mentalvenom <lukas.woodruff20@d155.org>
2020-12-31 16:05:25 -0600

    Update RPS.py

--- Cogs/RPS.py
+++ Cogs/RPS.py
@@ -1,33 +1,38 @@
 def setup(bot):
 	# Add the bot
 	bot.add_cog(RPS(bot))
+	
+class Turret(commands.Cog):
 
-@commands.command(description="Rock Paper Scissors")
-    async def rps(self, msg: str):
-        """Rock paper scissors. Example : /rps Rock if you want to use the rock."""
-        print('Rock Paper Scissors!')
-        # Les options possibles
-        t = ["rock", "paper", "scissors"]
-        # random choix pour le bot
-        computer = t[randint(0, 2)]
-        player = msg.lower()
-        print(msg)
-        if player == computer:
-            await self.bot.say("Tie!")
-        elif player == "rock":
-            if computer == "paper":
-                await self.bot.say("You lose! {0} covers {1}".format(computer, player))
-            else:
-                await self.bot.say("You win! {0} smashes {1}".format(player, computer))
-        elif player == "paper":
-            if computer == "scissors":
-                await self.bot.say("You lose! {0} cut {1}".format(computer, player))
-            else:
-                await self.bot.say("You win! {0} covers {1}".format(player, computer))
-        elif player == "scissors":
-            if computer == "rock":
-                await self.bot.say("You lose! {0} smashes {1}".format(computer, player))
-            else:
-                await self.bot.say("You win! {0} cut {1}".format(player, computer))
-        else:
-            await self.bot.say("That's not a valid play. Check your spelling!")
+    def __init__(self, bot):
+        self.bot = bot
+
+	@commands.command(description="Rock Paper Scissors")
+	    async def rps(self, msg: str):
+		"""Rock paper scissors. Example : /rps Rock if you want to use the rock."""
+		print('Rock Paper Scissors!')
+		# Les options possibles
+		t = ["rock", "paper", "scissors"]
+		# random choix pour le bot
+		computer = t[randint(0, 2)]
+		player = msg.lower()
+		print(msg)
+		if player == computer:
+		    await self.bot.say("Tie!")
+		elif player == "rock":
+		    if computer == "paper":
+			await self.bot.say("You lose! {0} covers {1}".format(computer, player))
+		    else:
+			await self.bot.say("You win! {0} smashes {1}".format(player, computer))
+		elif player == "paper":
+		    if computer == "scissors":
+			await self.bot.say("You lose! {0} cut {1}".format(computer, player))
+		    else:
+			await self.bot.say("You win! {0} covers {1}".format(player, computer))
+		elif player == "scissors":
+		    if computer == "rock":
+			await self.bot.say("You lose! {0} smashes {1}".format(computer, player))
+		    else:
+			await self.bot.say("You win! {0} cut {1}".format(player, computer))
+		else:
+		    await self.bot.say("That's not a valid play. Check your spelling!")
