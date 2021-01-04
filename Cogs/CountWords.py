483d9278fec3bee33b3c87177cc68fbaed096a1b
mentalvenom <lukas.woodruff20@d155.org>
2021-01-03 19:37:00 -0600

    Update CountWords.py

--- Cogs/CountWords.py
+++ Cogs/CountWords.py
@@ -23,7 +23,7 @@ class CountWords(commands.Cog):
           await message.channel.send(embed=embed)
           bot.countWords += 1
         @bot.command()
-        async def bruh(ctx):
+        async def countkek(ctx):
           embed=discord.Embed(title="kek counter", description="kek was said {} times!".format(ctx.bot.countWords) , color=0x00ffee)
           embed.set_thumbnail(url="https://emoji.gg/assets/emoji/4667_great_shame.png")
           embed.set_footer(text="tbh i don't care")
