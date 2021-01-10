b7d286df1cc5dfb40f0d128b279f3999b3168c7c
mentalvenom <lukas.woodruff20@d155.org>
2021-01-10 17:04:25 -0600

    Update SpamHandler.py

--- Cogs/SpamHandler.py
+++ Cogs/SpamHandler.py
@@ -1,6 +1,7 @@
-import discord
 from discord.ext import commands
 from AntiSpam import AntiSpamHandler
+import os
+import discord
 
 bot = commands.Bot(command_prefix="+")
 
@@ -13,8 +14,8 @@ class SpamHandler(commands.Cog):
         self.bot = bot
 
 warn_embed_dict = {
-    "title": "**Attention $USERNAME**",
-    "description": "You are being warned for spam",
+    "title": "**Dear $USERNAME**",
+    "description": "You are being warned for spam, please stop!",
     "timestamp": True,
     "color": 0xFF0000,
     "footer": {"text": "$BOTNAME", "icon_url": "$BOTAVATAR"},
@@ -24,10 +25,16 @@ warn_embed_dict = {
         {"name": "Current kicks:", "value": "$KICKCOUNT", "inline": False},
     ],
 }
-bot.handler = AntiSpamHandler(bot, ignore_bots=False, ignore_roles=["794690719888834561","770782107034845204","774077932963627009","774077162080436244"], guild_warn_message=warn_embed_dict)
+
+bot.handler = AntiSpamHandler(bot, ignore_bots=False, ignore_roles=["794690719888834561","770782107034845204","774077932963627009","774077162080436244"], warn_threshold=6, guild_warn_message=warn_embed_dict)
+
+@bot.event
+async def on_ready():
+    print(f"-----\nLogged in as: {bot.user.name} : {bot.user.id}\n-----")
 
 @bot.event
 async def on_message(message):
-    bot.handler.propagate(message)
+    await bot.handler.propagate(message)
     await bot.process_commands(message)
-    await user.add_roles("772566718438309890")
+    role = discord.utils.get(message.guild.roles, name="Muted")
+    await message.mentions[0].add_roles(role)
