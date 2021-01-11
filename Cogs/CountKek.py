36856328236cad95284f379bbf824197baf1c268
mentalvenom <lukas.woodruff20@d155.org>
2021-01-10 18:13:40 -0600

    Update CountKek.py

--- Cogs/CountKek.py
+++ Cogs/CountKek.py
@@ -18,8 +18,6 @@ class CountKek(commands.Cog):
         async def on_message(message):
             user = message.author
             if message.content.startswith("kek"):
-                embed=discord.Embed(title="Kek Counter", description=f"{user} said Kek!", color=0x00fffb)
-                await message.channel.send(embed=embed)
                 bot.message_counter += 1
             await bot.process_commands(message)
 
